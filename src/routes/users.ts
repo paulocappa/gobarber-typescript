24d9043f5c86898552a6a35e6d62309b8e1ed9bf
Paulo Messias <paulohenrique1403@yahoo.com>
2020-11-24 19:15:18 -0300

    feature image upload

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
 
+import authMiddleware from '../middlewares/auth';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -12,12 +18,19 @@ usersRouter.post('/', async (req, res) => {
 
     const user = await createUser.execute({ name, email, password });
 
-    delete user.password;
-
     return res.status(200).json(user);
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  authMiddleware,
+  upload.single('avatar'),
+  async (req, res) => {
+    return res.json({});
+  }
+);
 
 export default usersRouter;
