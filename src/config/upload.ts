0530773bb8584031f8f67bf47089f35b6a7b9a11
Paulo Messias <paulohenrique1403@yahoo.com>
2020-11-24 19:14:16 -0300

    add config to upload image

---
+++ src/config/upload.ts
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+export default {
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'tmp'),
+    filename: (req, file, callback) => {
+      const fileHash = crypto.randomBytes(16).toString('hex');
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
